8c0736dacafb7d9ed4ca365ccfc95d084de17668
KarlKrus <carl_son_cruz@live.com>
2016-11-09 11:55:46 +0800

    11.09.16.11.55.AM //UserPermitInsert

--- User/TypeCRUD.aspx.cs
+++ User/TypeCRUD.aspx.cs
@@ -11,6 +11,8 @@ namespace User
 {
     public partial class TypeCRUD : System.Web.UI.Page
     {
+        String Conn = ConfigurationManager.ConnectionStrings["_cnnstrngUserDBOne"].ConnectionString;
+        SqlConnection Connect = new SqlConnection(Conn);
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -28,8 +30,6 @@ namespace User
         protected void _lstvwTypeCRUD_ItemUpdating(object sender, ListViewUpdateEventArgs e)
         {
             TextBox USERTYPENAMETextBox = (TextBox)_lstvwTypeCRUD.EditItem.FindControl("USERTYPENAMETextBox");
-            String Conn = ConfigurationManager.ConnectionStrings["_cnnstrngUserDBOne"].ConnectionString;
-            SqlConnection Connect = new SqlConnection(Conn);
             string UserTypeName = USERTYPENAMETextBox.Text.Trim();
             string UserTypeID = e.Keys[0].ToString().Trim();
 
@@ -49,8 +49,6 @@ namespace User
         protected void _lstvwTypeCRUD_ItemInserting(object sender, ListViewInsertEventArgs e)
         {
             TextBox USERTYPENAMETextBox = (TextBox)_lstvwTypeCRUD.InsertItem.FindControl("USERTYPENAMETextBox");
-            String Conn = ConfigurationManager.ConnectionStrings["_cnnstrngUserDBOne"].ConnectionString;
-            SqlConnection Connect = new SqlConnection(Conn);
             string UserTypeName = USERTYPENAMETextBox.Text.Trim();
 
 
@@ -65,25 +63,19 @@ namespace User
 
             }
             Connect.Close();
-            //Connect.Open();
-            //Cmd = new SqlCommand("SELECT IDENT_CURRENT('USERTYPE')", Connect);
-            //SqlDataReader Reader = Cmd.ExecuteReader();
-            //while (Reader.Read())
-            //{
-            //    string TypeID = Reader.ToString();
-            //    ScriptManager.RegisterStartupScript(this, this.GetType(), "alertMessage", TypeID, true);
-            //}
-            //Connect.Close();
+            string scriptText = "alert('Record Inserted'); window.location='" + Request.ApplicationPath + "TypeList.aspx'";
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "alertMessage", scriptText, true);
         }
 
         protected void _lstvwTypeCRUD_ItemUpdated(object sender, ListViewUpdatedEventArgs e)
         {
-
+            int UserTypeID = Convert.ToInt32(Request.QueryString["id"]);
         }
 
         protected void _lstvwTypeCRUD_ItemInserted(object sender, ListViewInsertedEventArgs e)
         {
-            CheckBoxList _chkbxlstPermit = (CheckBoxList)_lstvwTypeCRUD.InsertItem.FindControl("");
+            int LastUserTypeID = 0;
+            CheckBoxList _chkbxlstPermit = (CheckBoxList)_lstvwTypeCRUD.InsertItem.FindControl("_chkbxlstPermit");
             String Conn = ConfigurationManager.ConnectionStrings["_cnnstrngUserDBOne"].ConnectionString;
             SqlConnection Connect = new SqlConnection(Conn);
 
@@ -92,42 +84,71 @@ namespace User
             SqlDataReader Reader = Cmd.ExecuteReader();
             while (Reader.Read())
             {
-                int LastUserTypeID =Convert.ToInt32(Reader.GetSqlValue(0).ToString());
-                Cmd = new SqlCommand("SELECT COUNT (*) FROM USERPERMIT WHERE USERTYPEID=@LastUserTypeID", Connect);
-                Cmd.Parameters.AddWithValue("@LastUserTypeID", LastUserTypeID);
-                Connect.Open();
-                Int32 Count = (Int32)Cmd.ExecuteScalar();
-                Connect.Close();
-                if (Count > 0)
+                LastUserTypeID = Convert.ToInt32(Reader.GetSqlValue(0).ToString());
+            }
+            Connect.Close();
+            for (int i=0; i<= _chkbxlstPermit.Items.Count - 1; i++)
+            {
+                if (_chkbxlstPermit.Items[i].Selected)
                 {
-                    for (int i = 0; i <= Count - 1; i++)
-                    {
-                        Connect.Open();
-                        Cmd = new SqlCommand("DELETE FROM USERPERMIT WHERE USERTYPEID=@LastUserTypeID", Connect);
-                        Cmd.Parameters.AddWithValue("@LastUserTypeID", LastUserTypeID);
-                        Cmd.ExecuteNonQuery();
-                        Connect.Close();
-
-                    }
-
-                    for (int i = 0; i <= _chkbxlstPermit.Items.Count - 1; i++)
-                    {
-                        if (_chkbxlstPermit.Items[i].Selected)
-                        {
-                            Connect.Open();
-                            Cmd = new SqlCommand("INSERT INTO WORKERTRADE (WORKERID,TRADEID) VALUES (@WorkerID,@TradeID)", Connect);
-                            Cmd.Parameters.AddWithValue("@WorkerID", WorkerID);
-                            Cmd.Parameters.AddWithValue("@TradeID", _chkbxlstPermit.Items[i].Value);
-                            Cmd.ExecuteNonQuery();
-                            Connect.Close();
-                        }
-
-                    }
-                    //string scriptText = "alert('Trade/s Modified'); window.location='" + Request.ApplicationPath + "TradeHandling.aspx'";
-                    //ScriptManager.RegisterStartupScript(this, this.GetType(), "alertMessage", scriptText, true);
-
+                    Connect.Open();
+                    Cmd = new SqlCommand("INSERT INTO USERPERMIT (USERTYPEID,PERMITID) VALUES (@LastUserTypeID,@PermitID)", Connect);
+                    Cmd.Parameters.AddWithValue("@LastUserTypeID", LastUserTypeID);
+                    Cmd.Parameters.AddWithValue("@PermitID", _chkbxlstPermit.Items[i].Value);
+                    Cmd.ExecuteNonQuery();
+                    Connect.Close();
                 }
-                Connect.Close();
+            }
+
+            //Connect.Close();
+            //Cmd = new SqlCommand("SELECT COUNT (*) FROM USERPERMIT WHERE USERTYPEID=@LastUserTypeID", Connect);
+            //Cmd.Parameters.AddWithValue("@LastUserTypeID", LastUserTypeID);
+            //Connect.Open();
+            //Int32 Count = (Int32)Cmd.ExecuteScalar();
+            //Connect.Close();
+            //if (Count > 0)
+            //{
+            //    for (int i = 0; i <= Count - 1; i++)
+            //    {
+            //        Connect.Open();
+            //        Cmd = new SqlCommand("DELETE FROM USERPERMIT WHERE USERTYPEID=@LastUserTypeID", Connect);
+            //        Cmd.Parameters.AddWithValue("@LastUserTypeID", LastUserTypeID);
+            //        Cmd.ExecuteNonQuery();
+            //        Connect.Close();
+
+            //    }
+
+            //    for (int i = 0; i <= _chkbxlstPermit.Items.Count - 1; i++)
+            //    {
+            //        if (_chkbxlstPermit.Items[i].Selected)
+            //        {
+            //            Connect.Open();
+            //            Cmd = new SqlCommand("INSERT INTO USERPERMIT (USERTYPEID,PERMITID) VALUES (@LastUserTypeID,@PermitID)", Connect);
+            //            Cmd.Parameters.AddWithValue("@LastUserTypeID", LastUserTypeID);
+            //            Cmd.Parameters.AddWithValue("@PermitID", _chkbxlstPermit.Items[i].Value);
+            //            Cmd.ExecuteNonQuery();
+            //            Connect.Close();
+            //        }
+
+            //    }
+            //    //string scriptText = "alert('Trade/s Modified'); window.location='" + Request.ApplicationPath + "TradeHandling.aspx'";
+            //    //ScriptManager.RegisterStartupScript(this, this.GetType(), "alertMessage", scriptText, true);
+            //}
+            //else
+            //{
+            //    for (int i = 0; i <= _chkbxlstPermit.Items.Count - 1; i++)
+            //    {
+            //        if (_chkbxlstPermit.Items[i].Selected)
+            //        {
+            //            Connect.Open();
+            //            Cmd = new SqlCommand("INSERT INTO USERPERMIT (USERTYPEID,PERMITID) VALUES (@LastUserTypeID,@PermitID)", Connect);
+            //            Cmd.Parameters.AddWithValue("@LastUserTypeID", LastUserTypeID);
+            //            Cmd.Parameters.AddWithValue("@PermitID", _chkbxlstPermit.Items[i].Value);
+            //            Cmd.ExecuteNonQuery();
+            //            Connect.Close();
+            //        }
+            //    }
+            //}
         }
     }
 }
